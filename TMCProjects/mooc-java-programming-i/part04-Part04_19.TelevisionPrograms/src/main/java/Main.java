b46c73f67b71ce0ddef56bbfeca5fdaf80d8862c
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-11 23:16:38 +0200

    04-19

--- TMCProjects/mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,30 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        String name;
+        int duration;
+
+        while (true) {
+            System.out.println("Name: ");
+            name = scanner.nextLine();
+
+            if (name.equals("")) {
+                break;
+            }
+
+            System.out.println("Duration: ");
+            duration = Integer.parseInt(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+        }
+
+        System.out.println("Program's maximum duration? ");
+        int max = scanner.nextInt();
+
+        for (TelevisionProgram p : programs) {
+            if (p.getDuration() <= max) {
+                System.out.println(p);
+            }
+        }
     }
 }
