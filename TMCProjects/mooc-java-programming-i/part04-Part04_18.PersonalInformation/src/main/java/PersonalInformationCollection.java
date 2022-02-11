78c4369003a1a28496ad945cb6e559d90df58f43
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-11 22:19:30 +0200

    04-18

--- TMCProjects/mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,27 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        String first = " ", second = " ", id = "";
+
+        while (true){
+            System.out.println("First name: ");
+            first = scanner.nextLine();
+
+            if(first.equals("")){
+                break;
+            }
+
+            System.out.println("Last name: ");
+            second = scanner.nextLine();
+
+            System.out.println("Identification number: ");
+            id = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(first, second, id));
+        }
+
+        for (PersonalInformation ps: infoCollection){
+            System.out.println(ps.getFirstName() + " " + ps.getLastName());
+        }
     }
 }
