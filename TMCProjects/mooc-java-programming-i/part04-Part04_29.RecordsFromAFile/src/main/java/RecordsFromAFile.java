7cfad08eb7be77ff8353fdb810174c439d9debfc
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-13 00:16:31 +0200

    04-29

--- TMCProjects/mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -5,7 +5,37 @@ import java.util.Scanner;
 public class RecordsFromAFile {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        Scanner scan = new Scanner(System.in);
+        String fileName = scan.nextLine();
+
+        try(Scanner file = new Scanner(Paths.get(fileName))){
+
+            while (file.hasNextLine()){
+                String[] j = file.nextLine().split(",");
+                String name = j[0];
+                int age = Integer.parseInt(j[1]);
+
+                String year = (age > 1) ? " years" : " year";
+
+                System.out.println(name + ", age: " + age + year);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
 
     }
 }
+
+/*
+Name of the file:
+data.txt
+lily, age: 3 years
+anton, age: 5 years
+levi, age: 4 years
+amy, age: 1 year
+
+NB! The word "year" should be formatted based on the age.
+ */
