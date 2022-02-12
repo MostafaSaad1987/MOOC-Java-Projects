a82a8d97278bd81a8a7e49ff4a87ef49a0ac5209
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-12 23:47:28 +0200

    04-27

--- TMCProjects/mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -13,5 +13,25 @@ public class IsItInTheFile {
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
 
+        Boolean found = false;
+
+        try (Scanner fil = new Scanner(Paths.get(file))) {
+
+            while (fil.hasNextLine()) {
+                if (fil.nextLine().equals(searchedFor)) {
+                    System.out.println("Found!");
+                    found = true;
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+
+        if (!found) {
+            System.out.println("Not found.");
+        }
+
     }
 }
