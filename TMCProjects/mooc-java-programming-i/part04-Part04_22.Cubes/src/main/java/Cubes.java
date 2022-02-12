58ebc66988d40ff96cdf9be1dde025cd0973a066
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-12 21:30:22 +0200

    Update Cubes.java

--- TMCProjects/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -16,18 +16,3 @@ public class Cubes {
         }
     }
 }
-
-/*
-Write a program that reads strings from the user until the user inputs the string "end".
-As long as the input is not "end",
-the program should handle the input as an integer and print the cube of the number provided (i.e., number _ number _ number).
-Below are some sample outputs
-
-3
-27
--1
--1
-11
-1331
-end
- */
