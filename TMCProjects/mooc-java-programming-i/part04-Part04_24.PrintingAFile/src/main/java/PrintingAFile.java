bb59c672cc81150b0cba083e80577d3ca2156e3c
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-12 22:48:33 +0200

    4-24

--- TMCProjects/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -6,5 +6,12 @@ public class PrintingAFile {
 
     public static void main(String[] args) {
 
+        try (Scanner file = new Scanner(Paths.get("data.txt"))) {
+            while (file.hasNextLine()) {
+                System.out.println(file.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
