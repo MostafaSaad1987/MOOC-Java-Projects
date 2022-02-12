f989f77e9810d5c3b1ba3eba55091d4a03698b87
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-12 22:53:53 +0200

    04-25

--- TMCProjects/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
+        System.out.println("Which file should have its contents printed?");
         Scanner scanner = new Scanner(System.in);
 
+        try (Scanner file = new Scanner(Paths.get(scanner.nextLine()))) {
+            while (file.hasNextLine()) {
+                System.out.println(file.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
