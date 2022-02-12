17d083ea6c916f19113c1c7dd3a13beb23a0a9eb
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-12 20:59:42 +0200

    04-21

--- TMCProjects/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,16 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
 
+        while (true) {
+            if(scanner.nextLine().equals("end")){
+                break;
+            } else {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
     }
 }
