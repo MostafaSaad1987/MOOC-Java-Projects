bed329bdd08f7f9fd62e1ee982ab69d1bdfd4788
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-12 23:55:12 +0200

    04-28

--- TMCProjects/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -14,6 +14,29 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        int counter = 0;
+
+        try (Scanner fil = new Scanner(Paths.get(file))) {
+            while (fil.hasNextLine()){
+                int j = Integer.parseInt(fil.nextLine());
+                if(j >= lowerBound && j <= upperBound){
+                    counter++;
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("Numbers: " + counter);
+
     }
 
 }
+
+/*
+Then the program reads the numbers contained in the file (each number is on its own line) and only accounts for the numbers which are inside the given range.
+Finally, the program should print the number of numbers that were inside the given range.
+
+You can convert a string-type integer read from a file into a proper integer using the command Integer.valueOf (just as when handling input from a user).
+Sample output
+ */
