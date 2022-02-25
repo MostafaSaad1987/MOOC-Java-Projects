fa4756717fe54e7b5105923fdbc47bed72e9f50a
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-02-25 17:32:13 +0200

    05-02

---
+++ TMCProjects/mooc-java-programming-i/part05-Part05_02.Book/src/main/java/Book.java
@@ -0,0 +1,28 @@
+
+public class Book {
+    private String author, title;
+    private int pageCount;
+
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.title = name;
+        this.pageCount = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return title;
+    }
+
+    public int getPages() {
+        return pageCount;
+    }
+
+    @Override
+    public String toString() {
+        return author + ", " + title + ", " + pageCount + " pages";
+    }
+}
