55e6789ea5aeff5b8b9b78173aa4c9af70d6213c
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-03-01 06:45:12 +0200

    05-09

--- TMCProjects/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,20 @@
 
 public class HealthStation {
 
+    private int numOfWeighs = 0;
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        numOfWeighs++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    //Misspelled from them. Not me.
+    public int weighings() {
+        return numOfWeighs;
     }
 
 }
