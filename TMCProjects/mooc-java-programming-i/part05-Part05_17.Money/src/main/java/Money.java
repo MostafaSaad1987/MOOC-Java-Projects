5d1f5073cdd258150035cc74a61d847edc2eed53
Armagdo <52860974+ArmagdoGaming@users.noreply.github.com>
2022-03-02 03:08:42 +0200

    05-17

--- TMCProjects/mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -32,4 +32,43 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        return new Money(this.euros + addition.euros, this.cents + addition.cents);
+    }
+
+    public boolean lessThan(Money compared) {
+        if (compared.euros > this.euros) {
+            return true;
+        } else {
+            return compared.euros == this.euros && compared.cents > this.cents;
+        }
+    }
+
+    public Money minus(Money decreaser) {
+        int newEuros = -1, newCents = -1;
+
+        if (this.euros - decreaser.euros >= 0) {
+            newEuros = this.euros - decreaser.euros;
+        }
+
+        if (this.cents - decreaser.cents >= 0) {
+            newCents = this.cents - decreaser.cents;
+        } else if (newEuros >= 0) {
+            if (this.cents == 0) {
+                newCents = Math.abs(100 - decreaser.cents);
+            } else {
+                newCents = Math.abs(this.cents - decreaser.cents);
+            }
+            newEuros--;
+        } else {
+            newCents = 0;
+        }
+
+        if (newEuros < 0) {
+            newEuros = 0;
+            newCents = 0;
+        }
+
+        return new Money(newEuros, newCents);
+    }
 }
